2832ce550b8c8e979b2749ecd5a24cce454de961
Vijit Kumar <vijitviku@gmail.com>
2022-02-12 17:06:16 +0530

    Third commit

--- Reco/urls.py
+++ Reco/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 
 from . import views
+
 urlpatterns = {
-    path('', views.index, name = "index")
+    path('', views.index, name = "index"),
+    path('WebDev', views.WebDev, name = "WebDev"),
+    path('CyberSec', views.CyberSec, name = "CyberSec"),
+    path('ML', views.ML, name = "ML"),
+    path('CPP', views.CPP, name = "CPP"),
+    path('DataAnalyst', views.DataAnalyst, name = "DataAnalyst"),
+    path('CompArch', views.CompArch, name = "CompArch"),
+
 }
--- Reco/views.py
+++ Reco/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {'name': 'VK'})
+
+def WebDev(request):
+    return render(request,'WebDev.html')
+
+def CyberSec(request):
+    return render(request,'CyberSec.html')
+
+def CompArch(request):
+    return render(request,'CompArch.html')
+
+def DataAnalyst(request):
+    return render(request,'DataAnalyst.html')
+
+def CPP(request):
+    return render(request,'CPP.html')
+
+def ML(request):
+    return render(request,'ML.html')
